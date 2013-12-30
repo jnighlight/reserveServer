f1ac334f687b118e9ca1f6201b9bc65ee72656a3
Mark Burton <mburton1@stetson.edu>
2013-12-30 06:21:30 -0500

    I've added a backup of the first programmed database. This database lacks support of equipment-accessory relationships. It also does not have any test data initialized yet.

---
+++ database_backups/diga_reservation_system.sql
@@ -0,0 +1,395 @@
+-- MySQL dump 10.13  Distrib 5.1.61, for redhat-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: diga_reservation_system
+-- ------------------------------------------------------
+-- Server version	5.1.61
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `building`
+--
+
+DROP TABLE IF EXISTS `building`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `building` (
+  `building_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`building_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `building`
+--
+
+LOCK TABLES `building` WRITE;
+/*!40000 ALTER TABLE `building` DISABLE KEYS */;
+/*!40000 ALTER TABLE `building` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `equipment`
+--
+
+DROP TABLE IF EXISTS `equipment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `equipment` (
+  `equipment_id` bigint(20) NOT NULL DEFAULT '0',
+  `serial_number` varchar(30) DEFAULT NULL,
+  `manufacturer` varchar(30) DEFAULT NULL,
+  `model_number` varchar(30) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`equipment_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipment`
+--
+
+LOCK TABLES `equipment` WRITE;
+/*!40000 ALTER TABLE `equipment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `equipment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `equipment_problem_report`
+--
+
+DROP TABLE IF EXISTS `equipment_problem_report`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `equipment_problem_report` (
+  `equipment_problem_report_id` bigint(20) NOT NULL DEFAULT '0',
+  `equipment_id` bigint(20) NOT NULL,
+  `reported_by_email` varchar(30) NOT NULL,
+  `date_time` datetime NOT NULL,
+  `problem_type_id` int(11) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`equipment_problem_report_id`),
+  KEY `equipment_id` (`equipment_id`),
+  KEY `reported_by_email` (`reported_by_email`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipment_problem_report`
+--
+
+LOCK TABLES `equipment_problem_report` WRITE;
+/*!40000 ALTER TABLE `equipment_problem_report` DISABLE KEYS */;
+/*!40000 ALTER TABLE `equipment_problem_report` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `equipment_reservation`
+--
+
+DROP TABLE IF EXISTS `equipment_reservation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `equipment_reservation` (
+  `equipment_reservation_id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(30) NOT NULL,
+  `equipment_id` bigint(20) NOT NULL,
+  `start_date_time` datetime NOT NULL,
+  `end_date_time` datetime NOT NULL,
+  PRIMARY KEY (`equipment_reservation_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipment_reservation`
+--
+
+LOCK TABLES `equipment_reservation` WRITE;
+/*!40000 ALTER TABLE `equipment_reservation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `equipment_reservation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `equipment_type`
+--
+
+DROP TABLE IF EXISTS `equipment_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `equipment_type` (
+  `equipment_type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`equipment_type_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipment_type`
+--
+
+LOCK TABLES `equipment_type` WRITE;
+/*!40000 ALTER TABLE `equipment_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `equipment_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `problem_type`
+--
+
+DROP TABLE IF EXISTS `problem_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `problem_type` (
+  `problem_type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`problem_type_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `problem_type`
+--
+
+LOCK TABLES `problem_type` WRITE;
+/*!40000 ALTER TABLE `problem_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `problem_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `room`
+--
+
+DROP TABLE IF EXISTS `room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `room` (
+  `room_id` int(11) NOT NULL AUTO_INCREMENT,
+  `room_number` int(11) NOT NULL,
+  `building_id` int(11) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`room_id`),
+  KEY `building_id` (`building_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room`
+--
+
+LOCK TABLES `room` WRITE;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `room_problem_report`
+--
+
+DROP TABLE IF EXISTS `room_problem_report`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `room_problem_report` (
+  `room_problem_report_id` int(11) NOT NULL AUTO_INCREMENT,
+  `room_id` int(11) NOT NULL,
+  `building_id` int(11) NOT NULL,
+  `reported_by_email` varchar(30) NOT NULL,
+  `date_time` datetime NOT NULL,
+  `problem_type_id` int(11) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`room_problem_report_id`),
+  KEY `room_id` (`room_id`),
+  KEY `building_id` (`building_id`),
+  KEY `reported_by_email` (`reported_by_email`),
+  KEY `problem_type_id` (`problem_type_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room_problem_report`
+--
+
+LOCK TABLES `room_problem_report` WRITE;
+/*!40000 ALTER TABLE `room_problem_report` DISABLE KEYS */;
+/*!40000 ALTER TABLE `room_problem_report` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `room_reservation`
+--
+
+DROP TABLE IF EXISTS `room_reservation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `room_reservation` (
+  `room_reservation_id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(30) NOT NULL,
+  `building_id` int(11) NOT NULL,
+  `room_id` int(11) NOT NULL,
+  `start_date_time` datetime DEFAULT NULL,
+  `end_date_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`room_reservation_id`),
+  KEY `email` (`email`),
+  KEY `room_id` (`room_id`),
+  KEY `building_id` (`building_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room_reservation`
+--
+
+LOCK TABLES `room_reservation` WRITE;
+/*!40000 ALTER TABLE `room_reservation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `room_reservation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `room_reservation_policy`
+--
+
+DROP TABLE IF EXISTS `room_reservation_policy`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `room_reservation_policy` (
+  `room_reservation_policy_id` int(11) NOT NULL AUTO_INCREMENT,
+  `building_id` int(11) NOT NULL,
+  `room_id` int(11) NOT NULL,
+  `reservable` tinyint(1) NOT NULL,
+  `max_reservation_hours` int(11) NOT NULL,
+  PRIMARY KEY (`room_reservation_policy_id`),
+  KEY `building_id` (`building_id`),
+  KEY `room_id` (`room_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room_reservation_policy`
+--
+
+LOCK TABLES `room_reservation_policy` WRITE;
+/*!40000 ALTER TABLE `room_reservation_policy` DISABLE KEYS */;
+/*!40000 ALTER TABLE `room_reservation_policy` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `email` varchar(30) NOT NULL DEFAULT '',
+  `password` varchar(30) NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `phone_number` varchar(15) NOT NULL,
+  `user_level_id` int(11) NOT NULL,
+  PRIMARY KEY (`email`),
+  KEY `user_level_id` (`user_level_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_level`
+--
+
+DROP TABLE IF EXISTS `user_level`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_level` (
+  `user_level_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(15) NOT NULL,
+  PRIMARY KEY (`user_level_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_level`
+--
+
+LOCK TABLES `user_level` WRITE;
+/*!40000 ALTER TABLE `user_level` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_level` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_warning`
+--
+
+DROP TABLE IF EXISTS `user_warning`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_warning` (
+  `user_warning_id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(30) NOT NULL,
+  `user_warning_type_id` int(11) NOT NULL,
+  `description` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`user_warning_id`),
+  KEY `email` (`email`),
+  KEY `user_warning_type_id` (`user_warning_type_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_warning`
+--
+
+LOCK TABLES `user_warning` WRITE;
+/*!40000 ALTER TABLE `user_warning` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_warning` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_warning_type`
+--
+
+DROP TABLE IF EXISTS `user_warning_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_warning_type` (
+  `user_warning_type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`user_warning_type_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_warning_type`
+--
+
+LOCK TABLES `user_warning_type` WRITE;
+/*!40000 ALTER TABLE `user_warning_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_warning_type` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-12-30  6:17:49
