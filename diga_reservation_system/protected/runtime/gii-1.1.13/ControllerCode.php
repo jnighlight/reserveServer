528ae783d4f11ae514a1684a9e218ac56ae9c566
Mark Burton <mburton1@stetson.edu>
2013-12-30 17:04:57 -0500

    Added a few test controllers and views.

--- diga_reservation_system/protected/runtime/gii-1.1.13/ControllerCode.php
+++ diga_reservation_system/protected/runtime/gii-1.1.13/ControllerCode.php
@@ -2,5 +2,5 @@
 return array (
   'template' => 'default',
   'baseClass' => 'Controller',
-  'actions' => 'addInventory',
+  'actions' => 'testAction2',
 );
---
+++ diga_reservation_system/protected/views/test/testAction1.php
@@ -0,0 +1,11 @@
+<?php
+/* @var $this TestController */
+
+$this->breadcrumbs=array(
+	'Test'=>array('/test'),
+	'TestAction1',
+);
+?>
+
+<h1> THIS WAS TOTALLY A TEST :D </h1>
+<p><?php echo CHtml::link('Link',array('test/testaction2'))?></p>
