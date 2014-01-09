8a860ef8aa9ad7aaf2b85d829e649120e4493de9
Mark Burton <mburton1@stetson.edu>
2014-01-09 15:45:24 -0500

    Added this view for admin equipment listings.

--- diga_reservation_system/protected/views/equipment/index.php
+++ diga_reservation_system/protected/views/equipment/index.php
@@ -1,157 +1,23 @@
 <?php
 /* @var $this EquipmentController */
+/* @var $dataProvider CActiveDataProvider */
 
 $this->breadcrumbs=array(
-	'Equipment',
-);
-/* Search form and filters */
-echo CHtml::openTag("div");
-  echo CHtml::beginForm();
-    echo "Search ".CHtml::activeTextField(Equipment::model(), "name");
-    echo CHtml::submitButton("Search");
-    $list = CHtml::listData($this->getAllEquipmentTypes(),
-			    "equipment_type_id","name");
-
-    echo CHtml::dropDownList("Types",EquipmentType::model(),
-	  $list, array("empty"=> "Select Type"));
-  echo CHtml::endForm();
-echo CHtml::closeTag("div");
-
-echo CHTML::closeTag("br");
-
-//$this->redirect(array("checkout/"));
-
-/*Equipment listing */
-
-$equipment = $this->getEquipment();
-
-$listContainer = array(
-  'id'=>'reservation_list',
+	'Equipments',
 );
 
-$listRow = array(
-  'class'=>'reservation_list_row',
+$this->menu=array(
+	array('label'=>'Create Equipment', 'url'=>array('create')),
+	array('label'=>'Manage Equipment', 'url'=>array('admin')),
 );
 
-$listImage = array(
-  'class' => 'reservation_list_image',
-);
+Yii::app()->clientScript->registerCssFile(Yii::app()->baseUrl."/css/crud_controls.css");
 
-$listDescription = array(
-  'class' => 'reservation_list_description',
-);
-
-$listDescriptionTitle = array(
-  'class' => 'reservation_list_description_title',
-);
-
-$listDescriptionDropDown = array(
-  'class' => 'reservation_list_dropdown',
-);
-
-$listDescriptionDropDownSection = array(
-  'class' => 'reservation_list_dropdown_section',
-);
-
-/*
-$equipmentSubmit = array(
-  'name' => 'equipment_id',
-);
-*/
-
-echo CHtml::beginForm();
-echo CHtml::openTag("div", $listContainer);
-
-for($x = 0; $x < sizeof($equipment); $x++)
-{
-  //New row
-  echo CHtml::openTag("div",$listRow);
-    //Equipment image
-    //echo CHtml::openTag("div",$listImage);
-      echo CHtml::image($equipment[$x]->image_url,"",$listImage);
-    //echo CHtml::closeTag("div"); // close image div
-    echo CHtml::openTag("div",$listDescription);
-      echo CHtml::openTag("p",$listDescriptionTitle);
-        echo $equipment[$x]->name;
-      echo CHtml::closeTag("p");
-      echo CHtml::openTag("p");
-        echo "<b>Type</b>: ".$this->getEquipmentType($equipment[$x]->equipment_type_id);
-      echo CHtml::closeTag("p");
-	/*
-      echo CHtml::openTag("p");
-        echo "<b>Description</b>: ".$equipment[$x]->description;
-      echo CHtml::closeTag("p");
-	*/
-      echo CHtml::openTag("p");
-        echo "<b>Availability</b>: "; //TODO: See if equipment item is available.
-      echo CHtml::closeTag("p");
-
-      echo CHtml::htmlButton("Checkout",
-	array(
-	  'type' => 'submit',
-  	  'name' => 'equipment_id',
-	  'value' => $equipment[$x]->equipment_id,
-	));
-
-	
-    echo CHtml::closeTag("div"); // close list description
-  echo CHtml::closeTag("div"); // close listRow
-
-  echo CHtml::openTag("div",$listDescriptionDropDown);
-  echo CHtml::openTag("div",$listDescription);
-  // Accessory Section --------------------------------
-  $accessories = $this->getAccessories($equipment[$x]->equipment_id);
-  echo CHtml::openTag("div",$listDescriptionDropDownSection);
-      echo CHtml::openTag("p",$listDescriptionTitle);
-        echo "Accessories";
-      echo CHtml::closeTag("p");
-  echo CHtml::openTag("ul");
-  for($y = 0; $y < sizeof($accessories); $y++)
-  {
-    echo CHtml::openTag("li");
-    echo $accessories[$y]->name;
-    echo CHtml::closeTag("li");
-  }
-  echo CHtml::closeTag("ul");
-
-  echo CHtml::closeTag("div"); // close accessory section
-  // End Accessory Section -----------------------------
-
-  // Specification Section
-  echo CHtml::openTag("div",$listDescriptionDropDownSection);
-    echo CHtml::openTag("p",$listDescriptionTitle);
-        echo "Specifications";
-      echo CHtml::closeTag("p");
-
-      $specs = $this->getSpecs($equipment[$x]->equipment_id);
-
-      echo CHtml::openTag("ul");
-      for($y = 0; $y < sizeof($specs); $y++)
-      {
-        echo CHtml::openTag("li");
-	  echo $specs[$y]->name.": ".$specs[$y]->value;
-	echo CHtml::closeTag("li");
-      }
-      echo CHtml::closeTag("ul");
-
-  echo CHtml::closeTag("div"); // close specifications section
-  // End Specification Section ------------------------------
-
-  // Description Section
-  echo CHtml::openTag("div",$listDescriptionDropDownSection);
-    echo CHtml::openTag("p",$listDescriptionTitle);
-      echo "<b>Description</b>: ";
-    echo CHtml::closeTag("p");
-    echo CHtml::openTag("p");
-      echo $equipment[$x]->description;
-    echo CHtml::closeTag("p");
-  echo CHtml::closeTag("div");
-  // End Description Section
-
-  echo CHtml::closeTag("div"); // close list description
-
-  echo CHtml::closeTag("div"); // close drop-down div
-}
-echo CHtml::closeTag("div");
-echo CHtml::endForm();
 ?>
+
+<h1>Equipment</h1>
+
+<?php $this->widget('zii.widgets.CListView', array(
+	'dataProvider'=>$dataProvider,
+	'itemView'=>'_view',
+)); ?>
