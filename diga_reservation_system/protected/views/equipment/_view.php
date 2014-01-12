59bedd2b373465f7332ff20d1c3783312105fcd6
Mark Burton <mburton1@stetson.edu>
2014-01-12 03:30:41 -0500

    Updated so that availability was rendered properly.

--- diga_reservation_system/protected/views/equipment/_view.php
+++ diga_reservation_system/protected/views/equipment/_view.php
@@ -9,8 +9,6 @@ $image_style = array('class'=>'equipment_image',);
 
 
 <div class="view">
-<!--	<b><?php echo CHtml::encode($data->getAttributeLabel('image_url')); ?>:</b> 
-        <?php echo CHtml::image($data->image_url); ?> -->
 	<?php echo CHtml::openTag("div",$image_style); ?>
 	<?php echo CHtml::link(CHtml::image($data->image_url), array('view', 'id'=>$data->equipment_id)); ?>
 	<?php echo CHtml::closeTag("div"); ?>
@@ -21,11 +19,6 @@ $image_style = array('class'=>'equipment_image',);
 	<?php echo CHtml::link(CHtml::encode($data->name), array('view', 'id'=>$data->equipment_id)); ?>
         <br />
 
-<!--
-	<b><?php echo CHtml::encode($data->getAttributeLabel('equipment_id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->equipment_id), array('view', 'id'=>$data->equipment_id)); ?>
-	<br />
--->
 	<b><?php echo CHtml::encode($data->getAttributeLabel('serial_number')); ?>:</b>
 	<?php echo CHtml::encode($data->serial_number); ?>
 	<br />
@@ -41,21 +34,19 @@ $image_style = array('class'=>'equipment_image',);
 	<b><?php echo CHtml::encode($data->getAttributeLabel('description')); ?>:</b>
 	<?php echo CHtml::encode($data->description); ?>
 	<br />
-<!--
-	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
-	<?php echo CHtml::encode($data->name); ?>
-	<br />
--->
-<!--
-	<b><?php echo CHtml::encode($data->getAttributeLabel('image_url')); ?>:</b>
-	<?php echo CHtml::encode($data->image_url); ?>
-	<br />
--->
-	<?php /*
-	<b><?php echo CHtml::encode($data->getAttributeLabel('equipment_type_id')); ?>:</b>
-	<?php echo CHtml::encode($data->equipment_type_id); ?>
-	<br />
 
-	*/ ?>
+	<b><?php echo CHtml::encode($data->getAttributeLabel('availability')); ?>:</b>
+        <?php
+          if($data->availability)
+	  {
+	    echo "<font color = 'green'>Available</font>";
+	  }
+	  else
+	  {
+	    echo "<font color = 'red'>Unavailable</font>";
+	  }
+          //echo CHtml::encode($data->availability);
 
+	?>
+        <br />
 </div>
