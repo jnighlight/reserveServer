cbcce888e92297552bb62e1b22abf72169b9c550
jake <jake@jake-VirtualBox.(none)>
2014-02-01 19:38:12 -0500

    updated room views

--- diga_reservation_system/protected/views/room/reserve.php
+++ diga_reservation_system/protected/views/room/reserve.php
@@ -3,153 +3,20 @@
 /* @var $model Room */
 /* @var $form CActiveForm */
 ?>
+
+<div class="form">
+
 <?php
 $build = $this -> getBuildings();
 $buildList = CHtml::listData($build, 'building_id', 'name');
-$buildMenu = CHtml::dropDownList('Building Names',Building::model(),$buildList, array('empty' => 'Choose a Building'));
-
-$availRooms = $this -> getRooms(4);
-//print($availRooms[0]['room_number']);
-$roomList = CHtml::listData($availRooms, 'room_id', 'room_number');
-$roomMenu = CHtml::dropDownList('Room Numbers', Room::model(), $roomList, array('empty' => 'Choose a Room Number'));
-
-/*Yii::app()->clientScript->registerScript(
-	Yii::app()->assetsManager->publish(Yii::app()->baseUrl . '/extensions/js/fullcalendar/fullcalendar/fullcalendar.min.js', CClientScript::POS_HEAD);//);*/
-
-$cs = Yii::app()->clientScript;
-$base = Yii::app()->baseUrl;
-$fullCal = '/extensions/js/fullcalendar';
-$cs->registerCssFile($base . $fullCal . '/fullcalendar/fullcalendar.css');
-$cs->registerCoreScript('jquery');
-$cs->registerScriptFile($base . $fullCal . '/lib/jquery-ui.custom.min.js');
-$cs->registerScriptFile($base . $fullCal . '/fullcalendar/fullcalendar.min.js');
-//$cs->registerScript();
+//$buildIDs = array_keys($buildList);
+//$roomsInBuild = $this -> getRooms($buildList[0]['building_id']);
 ?>
-<script type="text/javascript">
-
-	$(document).ready(function() {
-
-	
-
-		var date = new Date();
-
-		var d = date.getDate();
-
-		var m = date.getMonth();
-
-		var y = date.getFullYear();
-
-		
-
-		$('#calendar').fullCalendar({
-
-			editable: true,
-
-			events: [
-
-				{
-
-					title: 'All Day Event',
-
-					start: new Date(y, m, 1)
-
-				},
-
-				{
-
-					title: 'Long Event',
-
-					start: new Date(y, m, d-5),
-
-					end: new Date(y, m, d-2)
-
-				},
-
-				{
-
-					id: 999,
-
-					title: 'Repeating Event',
-
-					start: new Date(y, m, d-3, 16, 0),
-
-					allDay: false
-
-				},
-
-				{
-
-					id: 999,
-
-					title: 'Repeating Event',
-
-					start: new Date(y, m, d+4, 16, 0),
-
-					allDay: false
-
-				},
-
-				{
-
-					title: 'Meeting',
-
-					start: new Date(y, m, d, 10, 30),
-
-					allDay: false
-
-				},
-
-				{
-
-					title: 'Lunch',
-
-					start: new Date(y, m, d, 12, 0),
-
-					end: new Date(y, m, d, 14, 0),
-
-					allDay: false
-
-				},
-
-				{
-
-					title: 'Birthday Party',
-
-					start: new Date(y, m, d+1, 19, 0),
-
-					end: new Date(y, m, d+1, 22, 30),
-
-					allDay: false
-
-				},
-
-				{
-
-					title: 'Click for Google',
-
-					start: new Date(y, m, 28),
-
-					end: new Date(y, m, 29),
-
-					url: 'http://google.com/'
-
-				}
-
-			]
-
-		});
-
-		
-
-	});
-
-</script>
-<div id='calendar'></div>
-<div class="form">
 
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'room-reserve-form',
 	'enableAjaxValidation'=>false,
+	//'action'=>Yii::app()->createUrl('room/calendarRes'),
 )); ?>
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
@@ -157,15 +24,28 @@ $cs->registerScriptFile($base . $fullCal . '/fullcalendar/fullcalendar.min.js');
 	<?php echo $form->errorSummary($model); ?>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'description'); ?>
-		<?php echo $form->textField($model,'description'); ?>
-		<?php echo $form->error($model,'description'); ?>
+	<?php echo $form -> labelEx($model, 'building_id');
+		//echo CHtml::dropDownList('building_id','',$buildList,
+		echo $form -> dropDownList($model, 'building_id',$buildList,
+	array(
+	'empty'=> 'Choose a building',
+	'ajax' => array(
+	'type' => 'POST',
+	//'data' => array('building_id'=>'js:this.value'),
+	'url'=> CController::createUrl('room/reserve'),
+	//'update'=>'#room_num',
+	'update'=>'#' . CHtml::activeId($model, 'room_number'),
+	)));
+	 echo $form -> error($model, 'building_id'); ?>
 	</div>
-
 	<div class="row">
-		<?php echo $form->labelEx($model,'image_url'); ?>
-		<?php echo $form->textField($model,'image_url'); ?>
-		<?php echo $form->error($model,'image_url'); ?>
+	<?php
+	
+	echo $form -> labelEx($model, 'room_number');
+	//echo CHtml::dropDownList('room_num','',array(1=>'NY', 2=>'Paree'));
+	echo $form -> dropDownList($model,'room_number',array('empty'=>'select a building'));
+	echo $form -> error($model, 'room_number');
+?>
 	</div>
 
 
