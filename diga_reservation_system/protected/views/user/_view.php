8467eadf263d6e684ae83010f10655415a76c31b
Mark Burton <mburton1@stetson.edu>
2014-03-04 03:08:03 -0500

    Removed password and salt from listings.

--- diga_reservation_system/protected/views/user/_view.php
+++ diga_reservation_system/protected/views/user/_view.php
@@ -8,11 +8,11 @@
 	<b><?php echo CHtml::encode($data->getAttributeLabel('email')); ?>:</b>
 	<?php echo CHtml::link(CHtml::encode($data->email), array('view', 'id'=>$data->user_id)); ?>
 	<br />
-
+<!--
 	<b><?php echo CHtml::encode($data->getAttributeLabel('password')); ?>:</b>
 	<?php echo CHtml::encode($data->password); ?>
 	<br />
-
+-->
 	<b><?php echo CHtml::encode($data->getAttributeLabel('first_name')); ?>:</b>
 	<?php echo CHtml::encode($data->first_name); ?>
 	<br />
@@ -28,11 +28,11 @@
 	<b><?php echo CHtml::encode($data->getAttributeLabel('user_level_id')); ?>:</b>
 	<?php echo CHtml::encode($data->user_level_id); ?>
 	<br />
-
+<!--
 	<b><?php echo CHtml::encode($data->getAttributeLabel('salt')); ?>:</b>
 	<?php echo CHtml::encode($data->salt); ?>
 	<br />
-
+-->
 	<?php /*
 	<b><?php echo CHtml::encode($data->getAttributeLabel('id_number')); ?>:</b>
 	<?php echo CHtml::encode($data->id_number); ?>
