bce40a91985980a3b121c73658a4dc0eab2774d4
Mark Burton <mburton1@stetson.edu>
2014-03-04 20:53:03 -0500

    Passed the checkout history from the controller to the view.

--- diga_reservation_system/protected/controllers/Equipment_historyController.php
+++ diga_reservation_system/protected/controllers/Equipment_historyController.php
@@ -4,7 +4,32 @@ class Equipment_historyController extends Controller
 {
 	public function actionIndex()
 	{
-		$this->render('index');
+		if(!isset($_GET['equipment_id'])) // equipment_id not set
+                {
+                  print("Invalid Request!");
+                }
+                else // equipment__id is set
+                {
+                  if(empty($_GET['equipment_id']) || !is_numeric($_GET['equipment_id']))
+                  {
+                    print("Invalid Request!");
+                  }
+                  else if(Equipment::model()->findByPk($_GET['equipment_id']) == null)
+                  {
+                    //$this->render('index');
+                    print("We're sorry, the reservation you are looking for was not found.");
+                  }
+                  else // good to go
+                  {
+		    $equipment = Equipment::model()->findByPk($_GET['equipment_id']);
+		    $checkout_history = $equipment->getCheckouts();
+
+		    $this->render('index',
+                      array("equipment" => $equipment,
+			    "checkout_history" => $checkout_history,
+                        ));
+		  }
+		}
 	}
 
 	// Uncomment the following methods and override them if needed
