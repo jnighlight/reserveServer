f7c91b23a94968bf65bcb479bc51d0d219d8e0ac
Mark Burton <mburton1@stetson.edu>
2014-03-04 21:57:57 -0500

    Insert rendering code for checkin/checkout history.

--- diga_reservation_system/protected/views/equipment_history/index.php
+++ diga_reservation_system/protected/views/equipment_history/index.php
@@ -4,10 +4,67 @@
 $this->breadcrumbs=array(
 	'Equipment History',
 );
-?>
-<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
 
-<p>
-	You may change the content of this page by modifying
-	the file <tt><?php echo __FILE__; ?></tt>.
-</p>
+Yii::app()->clientScript->registerCssFile(Yii::app()->baseUrl."/css/equipment_history_page.css");
+
+$historyBlock = array(
+  'class'=>'history_block',
+);
+
+$historyBlockTitle = array(
+  'class'=>'history_block_title',
+);
+
+$checkoutHistoryBlock = array(
+  'class'=>'checkout_history_block',
+);
+
+$checkinHistoryBlock = array(
+  'class'=>'checkin_history_block',
+);
+
+//echo $equipment->name;
+
+echo CHtml::openTag("div", $historyBlock);
+
+// Checkout History Block
+
+echo CHtml::openTag("div", $checkoutHistoryBlock);
+
+echo CHtml::openTag("div", $historyBlockTitle);
+  echo CHtml::openTag("p");
+    echo "Checkout History";
+  echo CHtml::closeTag("p");
+echo CHtml::closeTag("div");
+
+foreach($checkout_history as $checkout)
+{
+  echo CHtml::openTag("p");
+    echo "<b>Start Date</b>: ".$checkout->start_date." &nbsp &nbsp &nbsp <b>End Date</b>:".$checkout->end_date;
+  echo CHtml::closeTag("p");
+}
+echo CHtml::closeTag("div");
+
+
+
+// Checkin History Block
+
+echo CHtml::openTag("div", $checkinHistoryBlock);
+
+echo CHtml::openTag("div", $historyBlockTitle);
+  echo CHtml::openTag("p");
+    echo "Checkin History";
+  echo CHtml::closeTag("p");
+echo CHtml::closeTag("div");
+
+foreach($checkin_history as $checkin)
+{
+  echo CHtml::openTag("p");
+    echo "<b>Checkin Date Time</b>: ".$checkin->checkin_date_time;
+  echo CHtml::closeTag("p");
+}
+echo CHtml::closeTag("div");
+
+
+echo CHtml::closeTag("div"); // history block closed
+?>
