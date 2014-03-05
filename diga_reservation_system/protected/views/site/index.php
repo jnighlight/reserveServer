6a105e402d8eefd7fa939558d522ab4204827897
Mark Burton <mburton1@stetson.edu>
2014-03-05 18:13:19 -0500

    Removed the now uneccessary list button and reordered the equipment buttons.

--- diga_reservation_system/protected/views/site/index.php
+++ diga_reservation_system/protected/views/site/index.php
@@ -15,23 +15,14 @@ echo CHTml::openTag("div", $mainButtons);
 echo CHtml::beginForm();
 // Equipment Reservation
 echo CHtml::imageButton(Yii::app()->baseUrl."/images/equipment_reservation.png",array('name'=>'reserve_equipment'));
-// Room Reservation
-/* if the user is an admin */
-echo CHtml::imageButton(Yii::app()->baseUrl."/images/room_reservation.png",array('name'=>'reserve_room'));
-if($this->isUserWorkStudy())
-{
-  // Equipment Admin Controls
+// Equipment Admin Controls
   echo CHtml::imageButton(Yii::app()->baseUrl."/images/equipment_admin.png",array('name'=>'equipment_admin_controls'));
-  // Daily Reports
-  echo CHtml::imageButton(Yii::app()->baseUrl."/images/daily_report.png",array('name'=>'daily_reports'));
-}
-if($this->isUserAdmin())
-{
+// Room Reservation
+echo CHtml::imageButton(Yii::app()->baseUrl."/images/room_reservation.png",array('name'=>'reserve_room'));
   //Room Admin Controls
   echo CHtml::imageButton(Yii::app()->baseUrl."/images/room_admin.png",array('name'=>'room_admin_controls'));
   // User Admin Mode
   echo CHtml::imageButton(Yii::app()->baseUrl."/images/users_admin_mode.png",array('name'=>'user_admin_controls'));
-}
 echo CHtml::endForm();
 echo CHtml::closeTag("div");
 
