82fa97873955c580728d39637fc6eeec56df543f
Mark Burton <mburton1@stetson.edu>
2014-03-06 19:05:43 -0500

    Coded GET variable processing and passed the appropriate user(should one be found) to the view.

--- diga_reservation_system/protected/controllers/User_historyController.php
+++ diga_reservation_system/protected/controllers/User_historyController.php
@@ -4,7 +4,30 @@ class User_historyController extends Controller
 {
 	public function actionIndex()
 	{
-		$this->render('index');
+	  if(!isset($_GET['user_id'])) // user_id not set
+                {
+                  print("Invalid Request!");
+                }
+                else // user_id is set
+                {
+                  if(empty($_GET['user_id']) || !is_numeric($_GET['user_id']))
+                  {
+                    print("Invalid Request!");
+                  }
+                  else if(User::model()->findByPk($_GET['user_id']) == null)
+                  {
+                    //$this->render('index');
+                    print("We're sorry, the user you are looking for was not found.");
+                  }
+                  else // good to go
+                  {
+                    $user = User::model()->findByPk($_GET['user_id']);
+
+                    $this->render('index',
+                      array("user" => $user,
+                        ));
+                  }
+                }
 	}
 
 	// Uncomment the following methods and override them if needed
